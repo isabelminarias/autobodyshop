f5ecd3d342ba249365d8cca61b8f511c61d9bbc5
Isabel M. Arias Salas <isabelminerva@outlook.com>
2018-01-31 18:00:42 -0400

    Login -- Dynamic changes between Registration, Login and Forgotten Password forms

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  loginclick: boolean = true;
+  registerclick: boolean = false;
+  passwordclick: boolean = false;
+  hello: string = 'showme';
+  constructor() {
+    
+  }
   ngOnInit() {
+  }
+
+  showregister() {
+    this.loginclick = false;
+    this.registerclick = true;
+    this.passwordclick = false;
+  }
+  showpassword() {
+    this.loginclick = false;
+    this.registerclick = false;
+    this.passwordclick = true;
+  }
+  showlogin() {
+    this.loginclick = true;
+    this.registerclick = false;
+    this.passwordclick = false;
   }
 
 }
